f2e50e9c8a8aed900f4460c11e3f86be4b57f481
esseeffeenne <stefanogigligigli@gmail.com>
2025-07-29 12:55:13 +0200

    Prevent post content loading in homepage

--- src/routes/+page.server.ts
+++ src/routes/+page.server.ts
@@ -2,7 +2,9 @@ import { getPosts } from '$lib/server/db/posts';
 import type { PageServerLoad } from './$types';
 
 export const load: PageServerLoad = async () => {
+  const posts = (await getPosts()) ?? [];
+  posts.map((post) => (post.content = ""));
   return {
-    posts: (await getPosts()) ?? [],
+    posts: posts
   };
 };
