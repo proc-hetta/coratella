e953b244b81a2780c94548421895c9500900847e
esseeffeenne <stefanogigligigli@gmail.com>
2025-08-26 10:02:18 +0200

    Fix markdown and latex rendering

--- src/lib/marked.ts
+++ src/lib/marked.ts
@@ -3,7 +3,15 @@ import markedKatex from 'marked-katex-extension';
 import { markedHighlight } from 'marked-highlight';
 import hljs from 'highlight.js';
 import { remark } from 'remark';
+import { unified } from 'unified';
 import remarkToc from 'remark-toc';
+import remarkGfm from 'remark-gfm';
+import remarkParse from 'remark-parse';
+import remarkMath from 'remark-math';
+import remarkRehype from 'remark-rehype';
+import remarkStringify from 'remark-stringify';
+import rehypeStringify from 'rehype-stringify';
+import rehypeKatex from 'rehype-katex';
 
 export function getMarkdownProcessors(useLatex: boolean, useToc: boolean) {
   const marked = new Marked(
@@ -21,11 +29,19 @@ export function getMarkdownProcessors(useLatex: boolean, useToc: boolean) {
     gfm: true,
   });
   if (useLatex) {
-    marked.use(markedKatex({
-      throwOnError: false,
-    }));
+    marked.use(
+      markedKatex({
+        throwOnError: false,
+      }),
+    );
   }
-  let textProcessor = useToc ? remark().use(remarkToc) : remark();
+  let textProcessor = remark().use(remarkGfm);
+  if (useToc) {
+    textProcessor.use(remarkToc);
+  }
+  if (useLatex) {
+    textProcessor.use(remarkMath).use(remarkRehype).use(rehypeKatex).use(rehypeStringify);
+  }
 
   return {
     marked: marked,
